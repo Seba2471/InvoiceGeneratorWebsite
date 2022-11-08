ec64c1f072c70e383ba76e071d46d174bbb0abd8
Sebastian <sebastian.siejak@gmail.com>
2022-11-08 18:44:48 +0100

    Added axios instances and input to downolad PDF document

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { downoladInvoiceFromData } from '../helpers/downoladInvoiceFromData';
 import { Invoice } from '../types/Invoice';
 import moment from 'moment';
 import InputText from './UI/Form/InputText';
@@ -36,15 +37,54 @@ export default function InvoiceForm() {
     },
   });
 
+  useEffect(() => {
+    setInvoice({
+      InvoiceNumber: '34/11/2022',
+      InvoiceItems: [
+        {
+          Name: 'test',
+          Quantity: 1,
+          Cost: 2000,
+          Value: 2000,
+        },
+      ],
+      SoldDate: moment().format(),
+      IssueDate: moment().format(),
+      Seller: {
+        FullName: 'Kupiec',
+        NIP: 'kupie NIP',
+        Address: {
+          Line1: 'kupie 1',
+          Line2: 'kupie 2',
+          Country: 'Polska',
+        },
+      },
+      Buyer: {
+        FullName: 'Kupiec',
+        NIP: 'kupie NIP',
+        Address: {
+          Line1: 'kupie 1',
+          Line2: 'kupie 2',
+          Country: 'Polska',
+        },
+      },
+    });
+  }, []);
+
+  const getPdf = async () => {
+    downoladInvoiceFromData(invoice);
+  };
+
   return (
     <div>
       <form>
         <InputText
           label='Numer faktury'
           value=''
-          onChange={(e: String) => console.log(e)}
+          onChange={(e: string) => console.log(e)}
         />
       </form>
+      <button onClick={async () => getPdf()}> Pobierz PDF </button>
     </div>
   );
 }
---
+++ src/helpers/downoladInvoiceFromData.tsx
@@ -0,0 +1,28 @@
+import { axiosInstance } from '../axios';
+import { Invoice } from '../types/Invoice';
+
+export const downoladInvoiceFromData = (data: Invoice) => {
+  try {
+    axiosInstance
+      .post('home', data, {
+        responseType: 'blob',
+      })
+      .then((response) => {
+        // create file link in browser's memory
+        const href = URL.createObjectURL(response.data);
+
+        // create "a" HTML element with href to file & click
+        const link = document.createElement('a');
+        link.href = href;
+        link.setAttribute('download', `Faktura-${data.InvoiceNumber}.pdf`); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+
+        // clean up "a" element & remove ObjectURL
+        document.body.removeChild(link);
+        URL.revokeObjectURL(href);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
