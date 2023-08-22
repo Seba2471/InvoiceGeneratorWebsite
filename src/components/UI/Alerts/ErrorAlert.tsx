37367bd113d258b84cd92c9c2fad6014afb08ab7
Sebastian <sebastian.siejak@gmail.com>
2023-08-22 14:19:39 +0200

    Centered error feedback

--- src/components/UI/Alerts/ErrorAlert.tsx
+++ src/components/UI/Alerts/ErrorAlert.tsx
@@ -3,8 +3,8 @@ import './Alert.scss';
 
 export default function ErrorAlert(props: { error: string }) {
   const errorMessage = (
-    <div className="alert alert-danger text-center">
-      <p className="alert__message">{props.error}</p>
+    <div className="alert alert-danger ">
+      <p className="alert__message text-center">{props.error}</p>
     </div>
   );
   return props.error ? errorMessage : null;
